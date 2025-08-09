3905cba656a9dc4db7eb2df6deb7f55f6b66ce86
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-09 19:04:15 -0300

    improve test runner presentation

--- src/test/java/dev/gutemberg/test/runner/TestRunner.java
+++ src/test/java/dev/gutemberg/test/runner/TestRunner.java
@@ -2,17 +2,56 @@ package dev.gutemberg.test.runner;
 
 import dev.gutemberg.test.runner.annotations.Test;
 import dev.gutemberg.test.runner.models.enums.Emoji;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static java.lang.System.out;
 
 public class TestRunner {
-    public static void main(final String[] args) {
-        out.printf("Starting tests %s\n", Emoji.ROCKET);
+    public static void main(final String[] args) throws Throwable {
+        try {
+            scanDirectoryAndRunTests("dev/gutemberg/expression/evaluator");
+        } catch (final InvocationTargetException e) {
+            out.println("--------------------------------");
+            throw e.getCause();
+        }
+        out.println("--------------------------------");
+        out.printf("Success %s\n", Emoji.PARTY_POPPER);
+    }
+
+    private static void scanDirectoryAndRunTests(final String directoryPath) throws InvocationTargetException {
+        try {
+            final var resources = TestRunner.class.getClassLoader().getResources(directoryPath);
+            while (resources.hasMoreElements()) {
+                final var element = resources.nextElement();
+                if (!element.getPath().contains("test")) {
+                    continue;
+                }
+                final var directory = new File(element.getFile());
+                final var files = Optional.ofNullable(directory.listFiles()).orElse(new File[]{});
+                for (final var file: files) {
+                    if (file.isFile()) {
+                        runTests(directoryPath, file.getName());
+                    } else {
+                        scanDirectoryAndRunTests(directoryPath + "/" + file.getName());
+                    }
+                }
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void runTests(final String directoryPath, String className) throws InvocationTargetException {
+        final var simpleClassName = className.replace(".class", "");
+        out.printf("%s %s\n", simpleClassName, Emoji.ROCKET);
         out.println("--------------------------------");
         try {
-            final var clazz = Class.forName("dev.gutemberg.expression.evaluator.evaluators.ExpressionEvaluatorTest");
+            final var fullClassName = directoryPath.replaceAll("/", ".") + "." + simpleClassName;
+            final var clazz = Class.forName(fullClassName);
             final var instance = clazz.getDeclaredConstructor().newInstance();
             final var tests = Arrays.stream(clazz.getMethods())
                     .filter(method ->
@@ -23,15 +62,13 @@ public class TestRunner {
             for (int i = 0; i < total; i++) {
                 out.printf("Running test %d/%d %s\n", i + 1, total, Emoji.TEST_TUBE);
                 tests.get(i).invoke(instance);
-                out.printf("Passed %s\n", Emoji.CHECK_MARK_BUTTON);
+                out.printf("  - Passed %s\n", Emoji.CHECK_MARK_BUTTON);
             }
         } catch (final InvocationTargetException e) {
-            out.printf("Failed %s\n", Emoji.NO_ENTRY);
-           throw new RuntimeException(e.getCause());
+            out.printf("  - Failed %s\n", Emoji.NO_ENTRY);
+            throw e;
         } catch (InstantiationException | IllegalAccessException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         } catch (final ClassNotFoundException ignored) {}
-        out.println("--------------------------------");
-        out.printf("Success %s\n", Emoji.PARTY_POPPER);
     }
 }
