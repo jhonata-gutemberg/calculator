c2042e727dc235bfa597fab1e43a0298f7899a6c
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-09 02:54:24 -0300

    add expression token wrapper

---
+++ src/main/java/dev/gutemberg/calculator/models/wrappers/ExpressionToken.java
@@ -0,0 +1,30 @@
+package dev.gutemberg.calculator.models.wrappers;
+
+import dev.gutemberg.calculator.models.enums.Operator;
+import dev.gutemberg.calculator.models.enums.Parentheses;
+
+public record ExpressionToken(Double operand, Operator operator, Parentheses parentheses) {
+    public static ExpressionToken from(final Double operand) {
+        return new ExpressionToken(operand, null, null);
+    }
+
+    public static ExpressionToken from(final Operator operator) {
+        return new ExpressionToken(null, operator, null);
+    }
+
+    public static ExpressionToken from(final Parentheses parentheses) {
+        return new ExpressionToken(null, null, parentheses);
+    }
+
+    public boolean isOperand() {
+        return operand != null;
+    }
+
+    public boolean isOperator() {
+        return operator != null;
+    }
+
+    public boolean isParentheses() {
+        return parentheses != null;
+    }
+}
